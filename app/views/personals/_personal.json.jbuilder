2ab277b5b151604cae9a2724f850e31375bfe853
jumpei-sogawa <jumpe123oon@gmail.com>
2018-03-26 18:17:01 +0900

    g scaffold personal

---
+++ app/views/personals/_personal.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! personal, :id, :user_id, :name, :kana, :sex, :birth, :created_at, :updated_at
+json.url personal_url(personal, format: :json)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180323060735) do
+ActiveRecord::Schema.define(version: 20180326091303) do
+
+  create_table "personals", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer "user_id"
+    t.string "name"
+    t.string "kana"
+    t.integer "sex"
+    t.date "birth"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "email", default: "", null: false

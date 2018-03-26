307577e361515fbe97eadf444b31950fdd257e8a
jumpei-sogawa <jumpe123oon@gmail.com>
2018-03-26 18:49:20 +0900

    home controller & mypage作成

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,8 @@ Rails.application.routes.draw do
 
   resources :personals
   resources :contacts
+
+  get '/home/mypage', to: 'home#mypage'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

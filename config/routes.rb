518ccef55acd5fa7e0931284fb6f21502b8bb15d
jumpei-sogawa <jumpe123oon@gmail.com>
2018-03-27 02:17:29 +0900

    g confirm action & routes

--- config/routes.rb
+++ config/routes.rb
@@ -14,9 +14,13 @@ Rails.application.routes.draw do
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
 
-  resources :personals
+  resources :personals do
+    collection do
+      post 'confirm'
+    end
+  end
 
   get '/home/mypage', to: 'home#mypage'
-  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

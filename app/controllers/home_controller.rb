0b981e8c9d7b90a1e790eba2ba1e870fa95dcac4
jumpei-sogawa <jumpe123oon@gmail.com>
2018-03-27 21:58:59 +0900

    you can edit personal

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,13 @@
 class HomeController < ApplicationController
+  before_action :set_personal
+
   def mypage
     @str  = "mypage"
-    @personal = Personal.find_by(user_id: current_user.id)
   end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_personal
+      @personal = Personal.find_by(user_id: current_user.id)
+    end
 end
